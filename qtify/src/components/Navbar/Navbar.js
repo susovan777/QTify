4d657518bf8edab686d64d5a40b2313052517f35
Susovan <susovan.sahoo777@gmail.com>
2024-02-19 14:30:10 +0530

    Added button component and styling

--- qtify/src/components/Navbar/Navbar.js
+++ qtify/src/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import "./navbar.css"
+import React from "react";
+import "./navbar.css";
 import Logo from "../../assets/logo.png";
+import Button from "../Button/Button";
 
 export default function Navbar() {
-    return (
-        <nav className='navbar'>
-            <img src={Logo} alt='QTify Logo' width={70} />
-            <input type='text' placeholder='Search a song of your choice' />
-            <button>Give Feedback</button>
-        </nav>
-    )
+  return (
+    <nav className="navbar">
+      <img src={Logo} alt="QTify Logo" width={70} />
+      <input type="text" placeholder="Search a song of your choice" />
+      <Button value={"Give Feedback"} />
+    </nav>
+  );
 }
