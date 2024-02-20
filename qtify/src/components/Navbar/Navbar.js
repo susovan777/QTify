a08121cc059c7874e64666c5ce17ef1f57914f93
Susovan <susovan.sahoo777@gmail.com>
2024-02-20 14:29:28 +0530

    Added Search button with an search icon

--- qtify/src/components/Navbar/Navbar.js
+++ qtify/src/components/Navbar/Navbar.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./navbar.css";
 import Logo from "../../assets/logo.png";
+import SearchIcon from "../../assets/search-icon.svg";
 import Button from "../Button/Button";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
       <img src={Logo} alt="QTify Logo" width={70} />
-      <input type="text" placeholder="Search a song of your choice" />
+      <div className="searchBox">
+        <input
+          type="text"
+          className="textbox"
+          placeholder="Search a song of your choice"
+        />
+        <button className="searchbtn"><img src={SearchIcon} width={20} alt="Search Icon" /></button>
+      </div>
       <Button value={"Give Feedback"} />
     </nav>
   );
