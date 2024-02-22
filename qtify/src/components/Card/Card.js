e72e32dfa3c9dbf18e9117d25bf965884f250edd
Susovan <susovan.sahoo777@gmail.com>
2024-02-22 15:08:58 +0530

    Added album card

--- qtify/src/components/Card/Card.js
+++ qtify/src/components/Card/Card.js
@@ -1,32 +1,47 @@
 import React from "react";
 import { Card, CardActionArea } from "@mui/material";
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 // import { CardActionArea } from '@mui/material';
+import Chip from "@mui/material/Chip";
 import Album from "../../assets/album1.png";
+import styles from "./card.module.css";
 
 function AlbumCard() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={Album}
-          alt="green iguana"
-        />
-        <CardContent>
-          {/* <Typography gutterBottom variant="h5" component="div">
+    <div className={styles.albumSection}>
+      <Typography variant="h6" color="white">
+        Top Album
+      </Typography>
+      <Card sx={{ maxWidth: 159 }} className={styles.cardDiv}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="170"
+            image={Album}
+            alt="green iguana"
+          />
+          <CardContent sx={{ padding: "5px" }}>
+            {/* <Typography gutterBottom variant="h5" component="div">
             Lizard
           </Typography>
           <Typography variant="body2" color="text.secondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
           </Typography> */}
-        </CardContent>
-      </CardActionArea>
-    </Card>
+            <Chip
+              size="small"
+              label="100 follows"
+              sx={{ bgcolor: "black", color: "white" }}
+            />
+          </CardContent>
+        </CardActionArea>
+      </Card>
+        <Typography variant="h6" color="white">
+          New Bollywood Songs
+        </Typography>
+    </div>
   );
 }
 
