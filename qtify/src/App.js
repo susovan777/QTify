69bc11261f75cc488ea2f15b0f446d247b01ad77
Susovan <susovan.sahoo777@gmail.com>
2024-02-29 19:31:51 +0530

    Created and Implemented generateFetchedData() to use fetched data from api.js

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,14 +1,35 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
-import AlbumCard from "./components/Card/Card";
+import MainCard from "./components/Card/Card";
+import { useEffect, useState } from "react";
+import { fetchNewAlbums, fetchSongs, fetchTopAlbums } from "./components/API/api";
 
 function App() {
+  const [data, setData] = useState({});
+
+  const generateFetchedData = async (key, source) => {
+    try {
+      const data = await source();
+      setData((prevState) => {
+        return {...prevState, [key]: data}
+      });
+      console.log(key, data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    generateFetchedData('topAlbums', fetchTopAlbums);
+    generateFetchedData('newAlbums', fetchNewAlbums);
+    generateFetchedData('songs', fetchSongs);
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <AlbumCard />
-      {/* <GetData /> */}
+      <MainCard />
     </div>
   );
 }
