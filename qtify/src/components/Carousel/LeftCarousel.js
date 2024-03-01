f0378206b995d9a89bd119b00a6fafb279d72831
Susovan <susovan.sahoo777@gmail.com>
2024-03-01 16:32:28 +0530

    Added Carousel component to navigate the cards

---
+++ qtify/src/components/Carousel/LeftCarousel.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { useSwiper } from "swiper/react";
+// import leftIcon from '../../assets/left.png'
+import { ReactComponent as LeftIcon } from "../../assets/leftarrow.svg";
+import style from "./Carousel.module.css";
+
+const CarouselLeftNavigation = () => {
+  const swiper = useSwiper();
+  const [isbegining, setIsbegining] = useState(swiper.isBeginning);
+
+  useEffect(() => {
+    swiper.on("slideChange", function () {
+      setIsbegining(swiper.isbegining);
+    });
+  }, [isbegining]);
+  return (
+    <div className={style.leftWrapper}>
+      {!isbegining ? <LeftIcon onClick={() => swiper.slidePrev()} /> : <></>}
+    </div>
+  );
+};
+
+export default CarouselLeftNavigation;
