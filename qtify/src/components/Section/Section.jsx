962941f2cbf0928a3202d31550831596fa7bc075
Susovan <susovan.sahoo777@gmail.com>
2024-03-01 16:23:05 +0530

    Modified Section component and stylings

--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import { Box, CircularProgress } from "@mui/material";
+import React, { useState } from "react";
+import styles from "./section.module.css";
+import MainCard from "../Card/Card";
 
-const Section = ({title, data, filterSource, type}) => {
-    return (
-        <div>
-            <div className={styles.header}>
-                <h3>{title}</h3>
+const Section = ({ title, data, filterSource, type }) => {
+  const [carouselToggle, setCarouselToggle] = useState(true);
+  const [filters, setFilters] = useState([{ key: "all", label: "all" }]);
+
+  const handleToggle = () => {
+    setCarouselToggle((prevState) => !prevState);
+  };
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <h3>{title}</h3>
+        <h4 className={styles.toggleText} onClick={handleToggle}>
+          {!carouselToggle ? "Collapse all" : "Show all"}
+        </h4>
+      </div>
+
+      {data.length === 0 ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <div className={styles.cardWrapper}>
+          {!carouselToggle ? (
+            <div className={styles.wrapper}>
+              {data.map((ele) => (
+                <MainCard data={ele} type={type} key={ele.id} />
+              ))}
             </div>
+          ) : (
+            ""
+          )}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
+
+export default Section;
