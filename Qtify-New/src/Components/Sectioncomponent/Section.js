ae16d1011e7948a3ff0da74296001edc28ed9bba
Susovan <susovan.sahoo777@gmail.com>
2024-03-31 00:00:00 +0530

    Added tab for song section and modified Section.js

--- Qtify-New/src/Components/Sectioncomponent/Section.js
+++ Qtify-New/src/Components/Sectioncomponent/Section.js
@@ -1,37 +1,67 @@
-import React from "react";
-import Card from "../Cardcomponent/Card";
-import "./Section.css";
-import { useState } from "react";
+import { Box, CircularProgress } from "@mui/material";
+import React, { useState } from "react";
+import styles from "./section.module.css";
+import MainCard from "../Cardcomponent/Card";
+import BasicTabs from "../Song Tab/tab";
 import Carousel from "../Carouselcomponent/Carousel";
 
+const Section = ({
+  title,
+  data = [],
+  filterSource,
+  type,
+  value = 0,
+  handleChange,
+}) => {
+  const [carouselToggle, setCarouselToggle] = useState(true);
+  // const [filters, setFilters] = useState([{ key: "all", label: "all" }]);
 
-const Section = ({ data, text }) => {
-  const [toCollapse, setToCollapse] = useState(true);
+  const handleToggle = () => {
+    setCarouselToggle((prevState) => !prevState);
+  };
+
   return (
-    <div className="section-wraper">
-      <div className="Section-parent">
-        <div className="Section-content">{text}</div>
-        <div
-          className="Section-content-1"
-          onClick={() => setToCollapse(!toCollapse)}
-        >
-          {toCollapse ? "Show all" : "Collapse"}
-        </div>
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <h3>{title}</h3>
+
+        {type === "song" ? (
+          <></>
+        ) : (
+          <h4 className={styles.toggleText} onClick={handleToggle}>
+            {!carouselToggle ? "Collapse all" : "Show all"}
+          </h4>
+        )}
+
       </div>
-      {toCollapse ? (
-        <Carousel data={data} swiperId={text} />
+
+      {type === "song" ? (
+        <BasicTabs value={value} handleChange={handleChange} />
       ) : (
-        <div className="Section-img">
-          {data.map((value) => {
-            return (
-              <Card
-                key={value.id}
-                title={value.title}
-                follows={value.follows}
-                image={value.image}
-              />
-            );
-          })}
+        <></>
+      )}
+
+      {data.length === 0 ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <div className={styles.cardWrapper}>
+          {!carouselToggle ? (
+            <div className={styles.wrapper}>
+              {data.map((ele) => (
+                <MainCard data={ele} type={type} key={ele.id} />
+              ))}
+            </div>
+          ) : (
+            <Carousel data={data} type={type} swiperId={title} />
+          )}
         </div>
       )}
     </div>
---
+++ Qtify-New/src/Components/Song Tab/tab.js
@@ -0,0 +1,66 @@
+import { Box, Tabs } from "@mui/material";
+import React from "react";
+import Tab from "@mui/material/Tab";
+import PropTypes from "prop-types";
+import Typography from "@mui/material/Typography";
+
+function TabPanel(prop) {
+  const { children, value, index, ...other } = prop;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>{<Typography>{children}</Typography>}</Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function allyProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs({ value, handleChange }) {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+          textColor=""
+          TabIndicatorProps={{
+            style: {
+              backgroundColor: "green",
+            },
+          }}
+        >
+          <Tab label="All" {...allyProps(0)} />
+          <Tab label="Rock" {...allyProps(1)} />
+          <Tab label="Pop" {...allyProps(2)} />
+          <Tab label="Jazz" {...allyProps(3)} />
+          <Tab label="Blues" {...allyProps(4)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}></TabPanel>
+      <TabPanel value={value} index={1}></TabPanel>
+      <TabPanel value={value} index={2}></TabPanel>
+      <TabPanel value={value} index={3}></TabPanel>
+      <TabPanel value={value} index={4}></TabPanel>
+    </Box>
+  );
+}
